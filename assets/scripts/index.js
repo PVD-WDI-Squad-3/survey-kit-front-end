8375181fbd45bdbd10bfc472d771fe1ab6cac6b4
acarlotto <acarlotto@cox.net>
2017-09-14 09:22:47 -0400

    Modified Index.html and Index.js
    - Added Popper.js file.
    - Put create an account form into a modal.
    - Added JavaScript/JQuery code to index.js so user is able to
    dynamically add unlimited number of answer fields to each question in a
    survey.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,34 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example')
+
+// tutorial from: http://blog.appliedinformaticsinc.com/how-to-addremove-input-fields-dynamically-with-jquery/
+$(document).ready(function () {
+  const maxFields = 20
+  const wrapper = $('.items')
+  const addButton = $('.add_field_button')
+
+  let x = 1
+  $(addButton).click(function (e) {
+    e.preventDefault()
+    if (x < maxFields) {
+      x++
+      $(wrapper).append('<div class="form-group"><label for="title"></label>' +
+'<input class="form-control col-md-11" id="answer" type="email" placeholder="" name="author"/>' +
+'<a href="#" class="remove_field"><i class="fa fa-times"></a></div>')
+    }
+  })
+
+  $(wrapper).on('click', '.remove_field', function (remove) {
+    remove.preventDefault()
+    $(this).parent('div').remove(); x--
+  })
+})
+
+$(() => {
+  $('#passChange').hide()
+  $('#log-out').hide()
+  $('#showChangePassButton').hide()
+  $('#close').hide()
+  $('#login').hide()
+})
